a0363c307e96fda4efc6ec94a643d8e6ed167cd6
Henry Linjamäki <henry.linjamaki@parmance.com>
2021-05-26 10:16:08 +0300

    computeSPIRKernelABIInfo(): Use SPIRABIInfo
    Change computeSPIRKernelABIInfo() use SPIRABIInfo instead of
    DefaultAPIInfo.

--- clang/lib/CodeGen/TargetInfo.cpp
+++ clang/lib/CodeGen/TargetInfo.cpp
@@ -10110,7 +10110,7 @@ void SPIRABIInfo::setCCs() {
 namespace clang {
 namespace CodeGen {
 void computeSPIRKernelABIInfo(CodeGenModule &CGM, CGFunctionInfo &FI) {
-  DefaultABIInfo SPIRABI(CGM.getTypes());
+  SPIRABIInfo SPIRABI(CGM.getTypes());
   SPIRABI.computeInfo(FI);
 }
 }
